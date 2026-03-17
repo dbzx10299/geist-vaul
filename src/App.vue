d1abd63dc898dace0eb31f2b675b44ae194c5467
David <dbzx-10299@hotmail.com>
2026-03-17 13:09:05 +0100

    feat: migrate to tsdown

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import DefaultDrawer from './components/DefaultDrawer.vue'
-import ScrollDrawer from './components/ScrollDrawer.vue'
-</script>
-
-<template>
-  <ScrollDrawer/>
-</template>
